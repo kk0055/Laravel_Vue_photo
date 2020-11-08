9f3bfb1e3cd65d9599d8b8bc22f73e657f2ca78d
kk0055 <rtsw.lgy@gmail.com>
2020-11-08 17:07:27 +0800

    ルートの定義

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
-require('./bootstrap');
+// require('./bootstrap');
+
+import Vue from 'vue'
+// router.jsをインポートする.ルーティングの定義
+import router from './router'
+// ルートコンポーネントをインポートする
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  router, // ルーティングの定義を読み込む
+  components: { App }, // ルートコンポーネントの使用を宣言する
+  template: '<App />' // ルートコンポーネントを描画する
+})
---
+++ resources/js/router.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// ページコンポーネントをインポートする
+import PhotoList from './pages/PhotoList.vue'
+import Login from './pages/Login.vue'
+
+// VueRouterプラグインを使用する
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    component: PhotoList
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+// VueRouterインスタンスを作成する
+const router = new VueRouter({
+  mode:'history',
+  routes
+})
+
+// VueRouterインスタンスをエクスポートする
+// app.jsでインポートするため
+export default router
