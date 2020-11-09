4c159d5e1ef9a73c4cf92b823758ca7e5643456a
kk0055 <rtsw.lgy@gmail.com>
2020-11-09 13:00:03 +0800

    login,register

---
+++ resources/js/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav class="navbar">
+<router-link class="navbar_brand" to="/">
+VuePhoto
+</router-link>
+    <div class="navbar__menu">
+      <div class="navbar__item">
+        <button class="button">
+          <i class="icon ion-md-add"></i>
+          Submit a photo
+        </button>
+      </div>
+      <span class="navbar__item">
+        username
+      </span>
+      <div class="navbar__item">
+        <RouterLink class="button button--link" to="/login">
+          Login / Register
+        </RouterLink>
+      </div>
+    </div>
+    </nav>
+  </div>
+</template>
