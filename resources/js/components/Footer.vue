8f4d4904e0ae131fe152aab0399f3f95667b6b18
kk0055 <rtsw.lgy@gmail.com>
2020-11-14 15:42:45 +0800

    vue done

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -10,18 +10,23 @@
   </footer>
 </template>
 <script>
+import { mapState, mapGetters } from 'vuex'
 export default {
   computed: {
-   isLogin() {
-     //['auth/check']は何？
-     return this.$store.getters['auth/check']
-   }
+     ...mapState({
+      apiStatus: state => state.auth.apiStatus
+    }),
+     ...mapGetters({
+      isLogin: 'auth/check'
+    })
   },
   methods: {
     async logout() {
       await this.$store.dispatch('auth/logout')
 
-      this.$router.push('/login')
+         if (this.apiStatus) {
+        this.$router.push('/login')
+      }
     }
   }
 }
